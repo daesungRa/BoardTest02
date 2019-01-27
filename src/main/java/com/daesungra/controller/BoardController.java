61756700108b55db0d7f7bbff6420cd1ac8f428b
daesungRa <daesungRa@gmail.com>
2019-01-28 08:59:13 +0900

    modify controller

--- src/main/java/com/daesungra/controller/BoardController.java
+++ src/main/java/com/daesungra/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.daesungra.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +21,11 @@ public class BoardController {
 	private BoardService boardService;
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
+	@ModelAttribute(value="cp")
+	public String getContextPath (HttpServletRequest request) {
+		return request.getContextPath(); 
+	}
+	
 	@RequestMapping(value="/test1", method=RequestMethod.GET)
 	public String test1 () {
 		logger.info("start test1");
@@ -30,14 +37,15 @@ public class BoardController {
 			logger.info("query fail");
 		}
 		
-		return "home";
+		return "index";
 	}
 	
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public ModelAndView boardList () {
+		logger.info("Move to Board List page");
 		ModelAndView mav = new ModelAndView();
-		
 		mav.setViewName("board/list");
+		
 		return mav;
 	}
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -14,8 +16,8 @@
 
 	<div id="container-fluid">
 		<h1>Welcome This Page!</h1>
-		<P>  The time on the server is ${serverTime}. </P>
-		<a href="/board/list">게시판 리스트 보기</a>
+		<P>  The time on the server is [${serverTime}]. </P>
+		<a href="board/list">게시판 리스트 보기</a>
 	</div>
 
 </body>
